41498cebab29af37a43e1a6959cdd8f3142ede51
Abysice <adambysice@gmail.com>
2014-12-19 19:24:37 -0500

    rustyattempt

--- stringpractice/palindrome.cpp
+++ stringpractice/palindrome.cpp
@@ -1,11 +1,39 @@
-/*
+/**************************************************
  * Simple paldindrome practice problem
- */
+ * Reads from command line, prints true/false
+ **************************************************/
+
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(int argc, char** argv) {
-  
-
-
-  return 0;
+int main(int argc, char** argv) 
+{	
+	//args into vector of strings
+  	std::vector<std::string> all_args(argv, argc + argv);
+	
+	if (argc > 1)
+	{
+	int head, tail;
+	for(int i = 1; i < argc; i++)
+	{
+		head = 0;
+		tail = all_args[i].length()-1;
+		for(int j = 0; j < (tail+1)/2; j++)
+		{
+			if (all_args[i].at(tail-j) != all_args[i].at(head+j))
+			{
+				printf("%s is not a palindrome \n", all_args[i].c_str());
+				break;
+			}
+			else if ((tail-j) - (head+j) <= 2)
+			{
+				printf("%s is a palindrome \n", all_args[i].c_str());
+				break;	
+			}
+		}	
+	}
+	}
+	
+	return 0;
 }
