d9734090b59c3ae7a0f1e17ea375a6b521672625
Abysice <adambysice@gmail.com>
2014-12-19 19:51:09 -0500

    array practice folder

---
+++ arraypractice/arrayfind.cpp
@@ -0,0 +1,15 @@
+/***************************************************
+ * Simple functions to look for things in arrays
+ * randomly generates arrays and finds things
+ ***************************************************/
+
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+
+
+
+	return 0;
+}
