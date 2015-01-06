d09eea1e70d5a567f9b72a6f72eb6ff24a5a27c8
Abysice <adambysice@gmail.com>
2015-01-06 09:09:32 -0500

    more

--- adtpractice/adtprob.cpp
+++ adtpractice/adtprob.cpp
@@ -48,7 +48,7 @@ public:
 	void insert(int key, int data)
 	{
 		int hash = (key % MAX_SIZE);
-
+		//no entry
 		if(table[hash] == NULL)
 		{
 			printf("inserted \n");
@@ -56,19 +56,16 @@ public:
 		}
 		else //collision
 		{
-			if(table[hash]->getKey() == key)
-			{
-					printf("key in use \n");
-					return;
-			}
-			hashNode * temp = table[hash]->next;
+			hashNode * temp = table[hash];
 			while(1)
-			{
+			{	
+				//key is in use
 				if (temp != NULL && temp->getKey() == key)
 				{
 					printf("key in use \n");
 					break;
 				}
+				//if there is node
 				if(temp == NULL)
 				{
 					printf("conflict 1 \n");
@@ -84,22 +81,61 @@ public:
 				else
 					temp = temp->next;
 			}
-				
+		}
+	}
+	void remove(int key)
+	{
+		int hash = key % MAX_SIZE;
+		hashNode * temp = table[hash];
+		if(temp == NULL)
+		{
+			printf("no entry matching key\n");
+			return;
+		}
+		else
+		{
+			temp = temp->next;
+			hashNode * last = table[hash];
+			if(key == last->getKey()) // first entry is the one
+			{
+				delete last;
+				printf("first entry removed\n");
+				table[hash] = temp;
+				return;
+			}
+
+			while(1)
+			{
+				if (temp == NULL)
+				{
+					printf("not found \n");
+					return;
+				}
+				else if(key == temp->getKey())
+				{
+					last->next = temp->next;
+					printf("later entry removed\n");
+					delete temp;
+					return;
+				}
+				temp = temp->next;
+
+			}
 		}
 
-
 	}
-
 };
 
 int main(int argc, char** argv)
 {
-
 	map test;
 	test.insert(1,20);
 	test.insert(129, 25);
 	test.insert(257, 25);
-	test.insert(257, 25);
-	test.insert(1,20);
+
+	test.remove(2);
+	test.remove(129);
+	test.remove(1);
+	test.remove(129);
 	return 0;
 }
